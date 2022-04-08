1e10ed41de1090300b987c60b6bd2b711c446d90
email <esayyasahmed1012@gmail.com>
2022-04-08 22:08:34 +0300

    quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * mian -entry point
+ *
+ * Return: always 0 (Success)
+ */
+int mian(void)
+{
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\", 59);
+return (1);
+}

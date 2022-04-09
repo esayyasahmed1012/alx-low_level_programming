de99ed2a36ed4ed1ec9bce894ca56ab362acff24
email <esayyasahmed1012@gmail.com>
2022-04-09 07:12:23 +0300

    print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entery point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+int digit;
+for (digit = 0; digit < 10; digit ++)
+{
+printf("%digit", digit);
+}
+printf("\n");
+return (0);
+}

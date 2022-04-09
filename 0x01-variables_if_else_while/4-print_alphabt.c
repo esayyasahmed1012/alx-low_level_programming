caabf661be61359fbc387eabce7e613aa1ffbe02
email <esayyasahmed1012@gmail.com>
2022-04-09 07:04:20 +0300

    print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - entry point 
+ * Return: alwasy 0(success)
+ */
+int main(void)
+{
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+if (alphabet == 'q')
+continue;
+else if (alphabet == 'e')
+continue;
+
+putchar(alphabet);
+}
+putchar('\n');
+return (0);
+}

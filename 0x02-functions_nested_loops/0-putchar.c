c04d49420637f6b4f0b7b1505a7098ac66be636f
email <esayyasahmed1012@gmail.com>
2022-04-12 20:18:47 +0300

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,13 +6,12 @@
  */
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
+char first[] = "_putchar";
+int i;
+for (int i = 0;i < 8; i++)
+{
+_putchar(first[i]);
+}
+_putchar('\n');
 return (0);
 }

2f34cfa7af3e2092c5c228f9d15db3b170813712
email <esayyasahmed1012@gmail.com>
2022-04-12 21:38:03 +0300

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * _isalpaha - returns 1 if the character is letter upper or lowercase else 0
+ * _isalpha - returns 1 if the character is letter upper or lowercase else 0
  * Return : 1 if it's letter otherwise 0
  * @c : recieve characters
  */
 int _isalpha(int c)
 {
-if ((c >= 'a' && c <= 'z')|| (c >= 'A' && c <= 'Z'))
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 {
 return (1);
 }

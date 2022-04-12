7384281650b065b82651828fe4ca5d81a701b74c
email <esayyasahmed1012@gmail.com>
2022-04-12 21:40:35 +0300

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * _isalpha - returns 1 if the character is letter upper or lowercase else 0
- * Return : 1 if it's letter otherwise 0
- * @c : recieve characters
+ * Return: 1 if it's letter otherwise 0
+ * @c: takes characters
  */
 int _isalpha(int c)
 {

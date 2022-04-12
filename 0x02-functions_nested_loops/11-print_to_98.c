a328f726136688a3b6214ea206e713d48696220a
email <esayyasahmed1012@gmail.com>
2022-04-12 23:31:28 +0300

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - function that prints all natural number b/n 0 to 898
+ * @n: takes an interger
+ */
+void print_to_98(int n)
+{
+int i;
+if (n > 98)
+{
+for (i = n; i > 97; i--)
+{
+printf("%d",i);
+if (i != 98)
+{
+printf(", ");
+}
+}
+}
+else 
+{
+for (i = n; i < 99; i++)
+{
+printf("%d",i);
+if (i != 98)
+{
+printf(", ");
+}
+}
+}
+printf("\n");
+}

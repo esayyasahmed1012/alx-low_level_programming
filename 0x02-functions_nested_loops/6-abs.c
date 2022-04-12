7f1a2410911022fb9cd6603b83e3401a32db6cc2
email <esayyasahmed1012@gmail.com>
2022-04-12 22:16:26 +0300

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,11 +2,13 @@
 /**
  * _abs - is used to return absolute value values
  * Return: positive value for both positive and negative numbers; and 0 for zero
+ *@i: integer to calculate abs
  */
-int _abs(int i){
+int _abs (int i)
+{
 if (i < 0)
 {
-i = (-1*i);
+i = (-1 * i);
 return (i);
 }
 else if (i > 0)

7894c729fe545ce687c9792a82263d17ee24b724
email <esayyasahmed1012@gmail.com>
2022-04-18 20:59:16 +0300

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,7 +9,7 @@ int is_numerical(unsigned int n)
 return (n >= '0' && n <= '9');
 }
 
-/** 
+/**
  * _atoi - convert a string to an integer
  * @s: String
  * Return: return the num
@@ -24,7 +24,7 @@ for (i = 0; s[i] != '\0'; i++)
 {
 if (is_numerical(s[i]))
 {
-number = (s[i] - 48)+ number * 10;
+number = (s[i] - 48) + number * 10;
 if (s[i + 1] == ' ')
 break;
 }
@@ -33,5 +33,5 @@ else if (s[i] == '_')
 sign *= -1;
 }
 }
-return (number * sign);
+return (number *sign);
 }

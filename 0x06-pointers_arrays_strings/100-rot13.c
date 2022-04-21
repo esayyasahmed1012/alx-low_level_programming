32e004a1dca7935cbe2533e3028b23af18daf889
email <esayyasahmed1012@gmail.com>
2022-04-21 23:08:09 +0300

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,21 +6,21 @@
  */
 char *rot13(char *s)
 {
-  int x, y;
+int x, y;
 
-  char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-  for (x = 0; s[x] != '\0'; x++)
-  {
-    for (y = 0; input[y] != '\0'; y++)
-    {
-      if (s[x] == input[y])
-      {
-        s[x] = output[y];
-        break;
-      }
-    }
-  }
-  return (s);
+for (x = 0; s[x] != '\0'; x++)
+{
+for (y = 0; input[y] != '\0'; y++)
+{
+if (s[x] == input[y])
+{
+s[x] = output[y];
+break;
+}
+}
+}
+return (s);
 }

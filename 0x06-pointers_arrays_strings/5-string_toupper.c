712cd8af12bf7f0081dfc59a9ada887bda158e9f
email <esayyasahmed1012@gmail.com>
2022-04-21 21:45:06 +0300

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * string_toupper - a function that changes all lowercase letters of a string t
+ * @s: pointer to input char
+ * Return: @s
+ */
+char *string_toupper(char *)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+if (s[i] >= 97 && s[i] <= 122)
+{
+s[i] = s[i] - 32;
+i++;
+}
+return (s);
+}

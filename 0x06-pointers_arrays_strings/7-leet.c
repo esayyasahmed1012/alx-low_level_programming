6723a06995cd5d53bb4b3e886c79cfdd6195c1e1
email <esayyasahmed1012@gmail.com>
2022-04-21 22:29:00 +0300

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -13,6 +13,12 @@ int n[] = {52, 51, 48, 55, 49};
 
 /*iterate values in array s*/
 while (s[c] != '\0')
+ /* loop through array value five times
+  * to check if the value is equal to
+  * any small letter in array sl or
+  * upper letter in array ul and if it
+  * is replace it with the value in array n
+  */
 {for (i = 0; i < 5; i++)
 {
 if (s[c] == sl[i] || s[c] == ul[i])

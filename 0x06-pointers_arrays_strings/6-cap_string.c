49f6b2e07090abf538fb7a87192efd84af2ba574
email <esayyasahmed1012@gmail.com>
2022-04-21 22:53:16 +0300

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,18 +13,18 @@ for (i = 0; s[i] != 0; i++)
 {
 if (i == 0; && s[i] >= 'a'; && s[i] <= 'z')
 {
-c[i] = c[i] - 32;
+s[i] = s[i] - 32;
 }
 for (j = 0; j < 13; j++)
 {
-if (c[i] == cha[j])
+if (s[i] == cha[j])
 {
-if (s[i + 1] >= 'a' && c[i + 1] <='z')
+if (s[i + 1] >= 'a' && s[i + 1] <='z')
 {
-c[i + 1] = c[i+1] - 32;
+s[i + 1] = s[i+1] - 32;
 }
 }
 }
 }
-return (c);
+return (s);
 }

7ed344b6132c1a16dfae2ae89c7c3445dc384d18
email <esayyasahmed1012@gmail.com>
2022-04-21 21:19:22 +0300

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,9 +11,9 @@ int i = 0, r;
 /* iterate through src and
  * compare it with dest
  */
-while(s1[i] != '\0' && s2[i] != '\0')
+while (s1[i] != '\0' && s2[i] != '\0')
 {
-if(s1[i] != s2[i])
+if (s1[i] != s2[i])
 {
 r = s1[i] - s2[i];
 break;

ce98bb53c0594198f6b4d759df93e93f3f17bda6
email <esayyasahmed1012@gmail.com>
2022-04-25 22:44:35 +0300

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,28 +1,49 @@
 #include "main.h"
 /**
- * _strcmp - a function that compares two strings
- * @s1: string 1 input to compare
- * @s2: against this other sting 2
- * Return: 0 if s1 and s2 are equal ,negative if the stopping char is s1, was
- */
+  * _strcmp - Compares two strings
+  * @s1: The first string
+  * @s2: The second string
+  *
+  * Return: int value
+  */
 int _strcmp(char *s1, char *s2)
 {
-int i = 0, r;
-/* iterate through src and
- * compare it with dest
- */
-while (s1[i] != '\0' && s2[i] != '\0')
-{
-if (s1[i] != s2[i])
-{
-r = s1[i] - s2[i];
-break;
-}
-else
-{
-r = s1[i] - s2[i];
-}
-i++;
-}
-return (r);
+	int a = 0, b = 0, c = 0, r = 0, lim;
+
+	while (s1[a])
+	{
+		a++;
+	}
+
+	while (s2[b])
+	{
+		b++;
+	}
+
+	if (a <= b)
+	{
+		lim = a;
+	}
+	else
+	{
+		lim = b;
+	}
+
+	while (c <= lim)
+	{
+		if (s1[c] == s2[c])
+		{
+			c++;
+			continue;
+		}
+		else
+		{
+			r = s1[c] - s2[c];
+			break;
+		}
+
+		c++;
+	}
+
+	return (r);
 }

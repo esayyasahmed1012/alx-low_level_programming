cc3cffefdd693d1fc81278d3d0b2fd44f467b483
email <esayyasahmed1012@gmail.com>
2022-04-29 22:50:22 +0300

    main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,4 +1,4 @@
-#ifndef "main.h"
+#ifndef main
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -19,5 +19,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
+#define main
 #endif

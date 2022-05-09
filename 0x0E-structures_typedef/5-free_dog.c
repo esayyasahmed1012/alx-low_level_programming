dcb15660c796d81176bb9969d4c9429946cca7b4
email <esayyasahmed1012@gmail.com>
2022-05-09 15:26:47 +0300

    5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,16 +2,17 @@
 #include "dog.h"
 
 /**
- * free_dog - initialize a variable of type struct dog
- * @d: dog's details
- */
-
+  * free_dog - ...
+  * @d: ...
+  *
+  * Return: ...
+  */
 void free_dog(dog_t *d)
 {
-	if (d)
+	if (d != NULL)
 	{
-		free(d->name);
 		free(d->owner);
+		free(d->name);
 		free(d);
 	}
 }
